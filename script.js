4b316ff95b66baff111f7952ce5c02410c2cb6f6
nik-ves <nik-ves@hotmail.com>
2021-07-30 12:32:58 +0200

    Fixed media queries and added some game logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// JS
+const checkBtn = document.querySelector('.check');
+
+const log = () => {
+    const inputFieldValue = Number(document.querySelector('.guess').value); 
+    console.log(inputFieldValue);
+    let message = document.querySelector('.message');
+
+    // if nothing is entered in input field
+    if (!inputFieldValue) {
+        message.textContent = 'No number!';
+    }
+}
+
+checkBtn.addEventListener('click', log);
