eff078edadaab87cb18643bae5afc63a0156c680
nik-ves <nik-ves@hotmail.com>
2021-07-31 16:36:27 +0200

    Game logic part 1

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const checkBtn = document.querySelector('.check');
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 const log = () => {
     const inputFieldValue = Number(document.querySelector('.guess').value); 
-    console.log(inputFieldValue);
+    const secretNumberBox = document.querySelector('.number');
+
     let message = document.querySelector('.message');
 
-    // if nothing is entered in input field
-    if (!inputFieldValue) {
-        message.textContent = 'No number!';
+    // number guessing logic
+    if (!inputFieldValue) { // if nothing is entered in input field
+        message.textContent = 'No number!'; 
+    } else if ( inputFieldValue === secretNumber) { // if number is correct
+        message.textContent = 'Correct number!';
+        document.body.style.backgroundColor = '#60b347';
+        secretNumberBox.textContent = secretNumber;
+    } else if (inputFieldValue < secretNumber) { // if number is lower than secret number
+        message.textContent = 'Too low!';
+    } else if (inputFieldValue > secretNumber) { // if number is higher than secret number
+        message.textContent = 'Too high!';
     }
+
 }
 
 checkBtn.addEventListener('click', log);
