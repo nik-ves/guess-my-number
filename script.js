e490eedbe88c0696806b5a332aaec23c14699aff
nik-ves <nik-ves@hotmail.com>
2021-07-31 17:47:58 +0200

    Refactoring some code

--- script.js
+++ script.js
@@ -2,54 +2,52 @@ const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+console.log(secretNumber)
 let score = 20;
 let highscore = 0;
 
-const log = () => {
+const checkNumber = () => {
     const inputFieldValue = Number(document.querySelector('.guess').value); 
     const secretNumberBox = document.querySelector('.number');
-    const scoreField = document.querySelector('.score');
-    const highscoreField = document.querySelector('.highscore');
-    
-    let message = document.querySelector('.message');
 
-    // number guessing logic
-    if (!inputFieldValue) { // if nothing is entered in input field
-        message.textContent = 'No number!'; 
-    } else if ( inputFieldValue === secretNumber) { // if number is correct
-        message.textContent = 'Correct number!';
-        document.body.style.backgroundColor = '#60b347';
-        secretNumberBox.textContent = secretNumber;
+    const displayMessage = (text) => {
+        const message = document.querySelector('.message');
+        message.textContent = text;
+    };
+
+    const checkScore = () => {
+        const scoreField = document.querySelector('.score');
+
+        if (score > 1) {
+            score--;
+            scoreField.textContent = score;
+        } else {
+            displayMessage('You lost!');
+            scoreField.textContent = 0;
+        }
+    };
+
+    const checkHighscore = () => {
+        const highscoreField = document.querySelector('.highscore');
 
         if (score > highscore) {
             highscore = score;
             highscoreField.textContent = score;
         }
-
-    } else if (inputFieldValue < secretNumber) { // if number is lower than secret number
-
-        if (score > 1) {
-            message.textContent = 'Too low!';
-            score--;
-            scoreField.textContent = score;
-        } else {
-            message.textContent = 'You lost!';
-            scoreField.textContent = 0;
-        }
-        
-    } else if (inputFieldValue > secretNumber) { // if number is higher than secret number
-
-        if (score > 1) {
-            message.textContent = 'Too high!';
-            score--;
-            scoreField.textContent = score;
-        } else {
-            message.textContent = 'You lost!';
-            scoreField.textContent = 0;
-        }
     };
-}
+
+    if (!inputFieldValue) {
+        displayMessage('No number!');
+    } else if ( inputFieldValue === secretNumber) {
+        displayMessage('Correct number!');
+        document.body.style.backgroundColor = '#60b347';
+        secretNumberBox.textContent = secretNumber;
+        checkHighscore();
+    } else if (inputFieldValue !== secretNumber) {
+        displayMessage(inputFieldValue > secretNumber ? 'Too high!' : 'Too low!');
+        checkScore();
+    };
+};
 
 const reset = () => {
     score = 20;
@@ -57,11 +55,10 @@ const reset = () => {
     const inputFieldValue = document.querySelector('.guess').value = ''; 
     const scoreField = document.querySelector('.score').textContent = score;
     const secretNumberBox = document.querySelector('.number').textContent = '?';
-
-    let message = document.querySelector('.message').textContent = 'Start guessing!';
+    const message = document.querySelector('.message').textContent = 'Start guessing!';
     
     document.body.style.backgroundColor = '#222';
-}
+};
 
-checkBtn.addEventListener('click', log);
+checkBtn.addEventListener('click', checkNumber);
 againBtn.addEventListener('click', reset);
